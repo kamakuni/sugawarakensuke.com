10c71688b4926a44347ec0d5966b4e2666124d38
kamakuni <superradio86@gmail.com>
2020-05-16 23:36:03 +0900

    update home page

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -8,22 +8,14 @@ const Home: FunctionalComponent = () => {
             <section className="hero is-medium">
                 <div className="hero-body">
                     <div className="container">
-                        <p>I'm Kensuke.</p>
-                    </div>
-                </div>
-            </section>
-            <section className="section">
-                <div className="container">
-                    <AsyncImage src="/assets/img/home.jpg" />
-                </div>
-            </section>
-            <section className="section tiles">
-                <div className="container">
-                    <div className="columns">
-                        <div className="column">
-                            <AsyncImage src="/assets/img/home.jpg" />
+                        <div className="columns">
+                            <div className="column">
+                                <p>I'm Kensuke.</p>
+                            </div>
+                            <div className="column">
+                                <AsyncImage src="/assets/img/home.jpg" />
+                            </div>
                         </div>
-                        <div className="column">Content2</div>
                     </div>
                 </div>
             </section>
