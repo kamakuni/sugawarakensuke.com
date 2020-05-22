67ce1d4859319d6f6de36054103ab8592e66291d
kamakuni <superradio86@gmail.com>
2020-05-22 23:41:06 +0900

    update home page

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -5,11 +5,6 @@ import AsyncImage from "../../components/asyncimage";
 const Home: FunctionalComponent = () => {
     return (
         <Fragment>
-            <section className="section">
-                <div className="container">
-                    <p>website</p>
-                </div>
-            </section>
             <section className="hero">
                 <div className="hero-body is-paddingless">
                     <AsyncImage src="/assets/img/home.jpg" />
