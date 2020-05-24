a559c381176efab5b2eed3b1acd37fba96b2ee15
kamakuni <superradio86@gmail.com>
2020-05-24 23:18:49 +0900

    update font-family

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -5,6 +5,9 @@ import AsyncImage from "../../components/asyncimage";
 const Home: FunctionalComponent = () => {
     return (
         <Fragment>
+            <section class="section">
+                <p style="font-family: '游明朝', YuMincho, 'Hiragino Mincho ProN W3', 'ヒラギノ明朝 ProN W3', 'Hiragino Mincho ProN', 'HG明朝E', 'ＭＳ Ｐ明朝', 'ＭＳ 明朝', serif;">こんにちは</p>
+            </section>
             <section className="hero">
                 <div className="hero-body is-paddingless">
                     <AsyncImage src="/assets/img/home.jpg" />
