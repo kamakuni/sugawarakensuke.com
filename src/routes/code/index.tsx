e65f5eee7afea70b0b6f4027426ec91577d18c64
kamakuni <superradio86@gmail.com>
2020-06-02 23:13:40 +0900

    add contents for Code page

--- src/routes/code/index.tsx
+++ src/routes/code/index.tsx
@@ -1,20 +1,32 @@
-import { FunctionalComponent, h } from "preact";
+import { FunctionalComponent, h, Fragment } from "preact";
 import "../../style/style.scss";
 import { Title } from "../../components/title";
 
 const Code: FunctionalComponent = () => {
     return (
-        <main>
+        <Fragment>
             <Title title="Code"></Title>
             <section className="section tiles">
                 <div className="container">
                     <div className="columns">
-                        <div className="column">Content1</div>
-                        <div className="column">Content2</div>
+                        <div className="column">
+                            <div class="container">
+                                <h4 className="title is-size-4">CHIP-8 Emulator</h4>
+                                <div className="contednt">
+                                    Virtual machine written in Golang
+                                </div>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <h4 className="title is-size-4">kmcc</h4>
+                            <div className="contednt">
+                                C language subsets compiler
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
-        </main>
+        </Fragment>
     );
 };
 
