a3b2f548c67c0c2c34bd28216437e92e28460439
kamakuni <superradio86@gmail.com>
2020-06-05 23:32:53 +0900

    update about page

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,18 +1,20 @@
-import { FunctionalComponent, h } from "preact";
+import { FunctionalComponent, h, Fragment } from "preact";
 import "../../style/style.scss";
 import { Title } from "../../components/title";
 
 export const About: FunctionalComponent = () => {
     return (
-        <main>
+        <Fragment>
             <Title title="About"></Title>
             <section className="section tiles">
                 <figure class="image container is-128x128">
                     <img class="is-rounded" src="/assets/img/face.jpg" />
                 </figure>
-                <div className="container"></div>
             </section>
-        </main>
+            <section className="section has-text-centered">
+                <p>Hello, I'm funny guy.</p>
+            </section>
+        </Fragment>
     );
 };
 
