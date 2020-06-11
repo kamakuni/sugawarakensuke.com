0c5a02f52e85b8ca8fda5fe082f23696833405a9
kamakuni <superradio86@gmail.com>
2020-06-11 23:48:44 +0900

    remove testing code

--- src/tests/header.test.tsx
+++ src/tests/header.test.tsx
@@ -1,7 +1,7 @@
 import { h } from "preact";
 // See: https://github.com/mzgoddard/preact-render-spy
 import { shallow } from "preact-render-spy";
-import Header from "../components/header";
+/*import Header from "../components/header";
 
 describe("Initial Test of the Header", () => {
     test("Header renders 3 nav items", () => {
@@ -10,3 +10,4 @@ describe("Initial Test of the Header", () => {
         expect(context.find("Link").length).toBe(3);
     });
 });
+*/
