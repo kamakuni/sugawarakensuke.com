6d77cc071004216be61816288650236c48c43bc4
kamakuni <superradio86@gmail.com>
2020-07-11 23:56:51 +0900

    added testing codes for the Header component

--- src/tests/header.test.tsx
+++ src/tests/header.test.tsx
@@ -1,3 +1,10 @@
 import { h } from "preact";
 import { Header } from "../components/header";
 import { shallow } from "enzyme";
+
+describe("Initial Test of Header", () => {
+    test("Header render 3 Link components", () => {
+        const context = shallow(<Header />);
+        expect(context.find("Link").length).toBe(3);
+    });
+});
