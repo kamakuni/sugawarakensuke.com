271e52800570b3e0027c323846abb8ca9f732117
kamakuni <superradio86@gmail.com>
2020-07-12 22:59:22 +0900

    added the directive for enzyme-adapter-preact-pure

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,3 +1,9 @@
+// This `<reference ...>` directive is necessary to include the adapter's
+// extensions to types in the "preact" and "enzyme" packages.
+
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="enzyme-adapter-preact-pure"/>
+
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-preact-pure";
 
