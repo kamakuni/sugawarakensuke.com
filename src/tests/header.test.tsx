187c8da27ce3afd01637c1334eb3e39edf07538a
kamakuni <superradio86@gmail.com>
2020-07-13 23:09:59 +0900

    updated package.json for testing

--- src/tests/header.test.tsx
+++ src/tests/header.test.tsx
@@ -1,10 +1,10 @@
 import { h } from "preact";
 import { Header } from "../components/header";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 
 describe("Initial Test of Header", () => {
-    test("Header render 3 Link components", () => {
-        const context = shallow(<Header />);
+    it("Header render 3 Link components", () => {
+        const context = mount(<Header />);
         expect(context.find("Link").length).toBe(3);
     });
 });
