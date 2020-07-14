ad8ff3e0fea319ed5e94fc1cbf8ce37688665d59
kamakuni <superradio86@gmail.com>
2020-07-14 22:49:59 +0900

    changed test runner

--- src/tests/header.test.tsx
+++ src/tests/header.test.tsx
@@ -1,9 +1,10 @@
+import { expect } from "chai";
 import { h } from "preact";
 import { Header } from "../components/header";
 import { mount } from "enzyme";
 
 describe("Initial Test of Header", () => {
-    it("Header render 3 Link components", () => {
+    it("should render 3 Link components", () => {
         const context = mount(<Header />);
         expect(context.find("Link").length).toBe(3);
     });
