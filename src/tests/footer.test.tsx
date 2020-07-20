ccf0735690c0ddfc9ba563e94a53dd22fc56cd3c
kamakuni <superradio86@gmail.com>
2020-07-20 22:13:40 +0900

    added test for Footer

---
+++ src/tests/footer.test.tsx
@@ -0,0 +1,10 @@
+import { h } from "preact";
+import { Footer } from "../components/footer";
+import { mount } from "enzyme";
+
+describe("Initial Test of Footer", () => {
+    it("should render 3 SnsLink components", () => {
+        const context = mount(<Footer />);
+        expect(context.find("SnsLink").length).toBe(3);
+    });
+});
