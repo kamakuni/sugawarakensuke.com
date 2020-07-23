8aa087b8cc99cc845b710765195dba742f040b5a
kamakuni <superradio86@gmail.com>
2020-07-23 23:00:26 +0900

    added tests for Code component

---
+++ src/tests/routes/code.test.tsx
@@ -0,0 +1,10 @@
+import { h } from "preact";
+import { Code } from "../../routes/code";
+import { mount } from "enzyme";
+
+describe("Initial Test of Code", () => {
+    it("should render 3 images", () => {
+        const context = mount(<Code />);
+        expect(context.find("img").length).toBe(3);
+    });
+});
