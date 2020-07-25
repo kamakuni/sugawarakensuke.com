01facbc08e6ac24e901af1559e23d8ff36b573fa
kamakuni <superradio86@gmail.com>
2020-07-25 22:22:11 +0900

    added tests for NotFound component

---
+++ src/tests/routes/notfound.test.tsx
@@ -0,0 +1,10 @@
+import { h } from "preact";
+import { NotFound } from "../../routes/notfound";
+import { mount } from "enzyme";
+
+describe("Initial Test of NotFound", () => {
+    it("should render Link", () => {
+        const context = mount(<NotFound />);
+        expect(context.find("Link").length).toBe(1);
+    });
+});
