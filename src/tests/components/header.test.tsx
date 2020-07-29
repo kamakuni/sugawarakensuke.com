b98b8e7eac634f5eef018d6d6a55d23a597dd3e3
kamakuni <superradio86@gmail.com>
2020-07-29 22:49:06 +0900

    added a test case for About

--- src/tests/components/header.test.tsx
+++ src/tests/components/header.test.tsx
@@ -9,7 +9,6 @@ describe("Initial Test of Header", () => {
     });
     it("should be active", () => {
         const context = mount(<Header />);
-        console.log(context.instance().state);
         //expect(context.instance().state("isActive")).toBe(true);
     });
 });
--- src/tests/routes/about.test.tsx
+++ src/tests/routes/about.test.tsx
@@ -1,12 +1,13 @@
 import { h } from "preact";
 import { About } from "../../routes/about";
+import { Title } from "../../components/title";
 import { mount } from "enzyme";
 
 describe("Initial Test of About", () => {
-    /*it("should render an Title", () => {
+    it("should render an Title", () => {
         const context = mount(<About />);
-        expect(context.find("Title").length).toBe(1);
-    });*/
+        expect(context.find(Title).length).toBe(1);
+    });
     it("should render an image", () => {
         const context = mount(<About />);
         expect(context.find("img").length).toBe(1);
