d59e5fdaa6517abe9ee6f6ced06a9388dbb00d12
kamakuni <superradio86@gmail.com>
2020-07-29 22:45:22 +0900

    updated a test for Home

--- src/tests/routes/home.test.tsx
+++ src/tests/routes/home.test.tsx
@@ -1,10 +1,11 @@
 import { h } from "preact";
 import Home from "../../routes/home";
+import { AsyncImage } from "../../components/asyncimage";
 import { mount } from "enzyme";
 
 describe("Initial Test of Home", () => {
     it("should render a AsyncImage", () => {
         const context = mount(<Home />);
-        expect(context.find("AsyncImage").length).toBe(1);
+        expect(context.find(AsyncImage).length).toBe(1);
     });
 });
