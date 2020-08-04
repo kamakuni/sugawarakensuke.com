26c383f636d0faee42d06f366b342298c6d6f6e4
kamakuni <superradio86@gmail.com>
2020-08-04 22:58:42 +0900

    change the mode of routering

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,6 @@
 import { FunctionalComponent, h } from "preact";
-import { Route, Router, RouterOnChangeArgs } from "preact-router";
+import { Route, Router } from "preact-router";
+import { createHashHistory } from 'history';
 import "../style/style.scss";
 import NotFoundPage from "../routes/notfound";
 import { Header } from "./header";
@@ -15,15 +16,16 @@ if ((module as any).hot) {
 }
 
 const App: FunctionalComponent = () => {
-    let currentUrl: string;
+    /*let currentUrl: string;
     const handleRoute = (e: RouterOnChangeArgs) => {
         currentUrl = e.url;
-    };
+    };*/
 
     return (
         <div id="app has-background-white">
             <Header />
-            <Router onChange={handleRoute}>
+            {/* <Router onChange={handleRoute} > */}
+            <Router history={createHashHistory()} >
                 <Route path="/" component={Home} />
                 <Route path="/code" component={Code} />
                 <Route path="/about" component={About} />
