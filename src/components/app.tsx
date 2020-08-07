2d5c9014f55e9d5e214c2b26fdc5f7ef8978dcc4
kamakuni <superradio86@gmail.com>
2020-08-07 20:41:24 +0900

    update testing

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,6 @@
 import { FunctionalComponent, h } from "preact";
 import { Route, Router } from "preact-router";
-import { createHashHistory } from 'history';
+import { createHashHistory } from "history";
 import "../style/style.scss";
 import NotFoundPage from "../routes/notfound";
 import { Header } from "./header";
@@ -16,16 +16,11 @@ if ((module as any).hot) {
 }
 
 const App: FunctionalComponent = () => {
-    /*let currentUrl: string;
-    const handleRoute = (e: RouterOnChangeArgs) => {
-        currentUrl = e.url;
-    };*/
 
     return (
         <div id="app has-background-white">
             <Header />
-            {/* <Router onChange={handleRoute} > */}
-            <Router history={createHashHistory()} >
+            <Router history={createHashHistory()}>
                 <Route path="/" component={Home} />
                 <Route path="/code" component={Code} />
                 <Route path="/about" component={About} />
--- src/tests/components/asyncimage.test.tsx
+++ src/tests/components/asyncimage.test.tsx
@@ -3,7 +3,7 @@ import { AsyncImage } from "../../components/asyncimage";
 import { mount } from "enzyme";
 
 describe("Initial Test of AsyncImage", () => {
-    it("should render title props", () => {
+    it("should render an element for loading", () => {
         const context = mount(<AsyncImage src="/assets/img/home.jpg" />);
         expect(context.find("div.loading").length).toBe(1);
     });
