318339f8e5235abf5b469cbf83667fe0da71cf42
kamakuni <superradio86@gmail.com>
2020-08-08 22:54:02 +0900

    add test for Header

--- src/tests/components/header.test.tsx
+++ src/tests/components/header.test.tsx
@@ -3,6 +3,12 @@ import { Header } from "../../components/header";
 import { mount } from "enzyme";
 
 describe("Initial Test of Header", () => {
+    it("should render an icon image", () => {
+        const context = mount(<Header />);
+        expect(context.find("img.is-rounded").prop("src")).toBe(
+            "/assets/img/face.jpg"
+        );
+    });
     it("should render 3 Link components", () => {
         const context = mount(<Header />);
         expect(context.find("Link").length).toBe(3);
