978ac21a9d0f034cbaccc8285e2834a6bf748be1
kamakuni <superradio86@gmail.com>
2020-08-16 22:08:49 +0900

    added a test to check Title in Home

--- src/tests/routes/home.test.tsx
+++ src/tests/routes/home.test.tsx
@@ -2,8 +2,13 @@ import { h } from "preact";
 import Home from "../../routes/home";
 import { AsyncImage } from "../../components/asyncimage";
 import { mount } from "enzyme";
+import { Title } from "../../components/title";
 
 describe("Initial Test of Home", () => {
+    it("should render a Title", () => {
+        const context = mount(<Home />);
+        expect(context.find(Title).length).toBe(1);
+    });
     it("should render a AsyncImage", () => {
         const context = mount(<Home />);
         expect(context.find(AsyncImage).length).toBe(1);
