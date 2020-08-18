45c775ddea39c4305b0c6dd11ad2464055b77a4b
kamakuni <superradio86@gmail.com>
2020-08-18 23:11:55 +0900

    added the test to check the title value in the About page

--- src/tests/routes/about.test.tsx
+++ src/tests/routes/about.test.tsx
@@ -8,6 +8,11 @@ describe("Initial Test of About", () => {
         const context = mount(<About />);
         expect(context.find(Title).length).toBe(1);
     });
+    it("should render a Title", () => {
+        const context = mount(<About />);
+        const title = context.find(Title);
+        expect(title.find("p").text()).toBe("About");
+    });
     it("should render an image", () => {
         const context = mount(<About />);
         expect(context.find("img").length).toBe(1);
