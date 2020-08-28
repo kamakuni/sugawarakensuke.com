8e3b887255ab1c4b4b01db6b163a15cd504c2c81
kamakuni <superradio86@gmail.com>
2020-08-28 22:57:01 +0900

    updated the test code for an image in the About page

--- src/tests/routes/about.test.tsx
+++ src/tests/routes/about.test.tsx
@@ -13,9 +13,11 @@ describe("Initial Test of About", () => {
         const title = context.find(Title);
         expect(title.find("p").text()).toBe("About");
     });
-    it("should render an image", () => {
+    it("should render an rounded image of my face", () => {
         const context = mount(<About />);
-        expect(context.find("img").length).toBe(1);
+        expect(context.find("img.is-rounded").prop("src")).toBe(
+            "/assets/img/face.jpg"
+        );
     });
     it("should have a content", () => {
         const context = mount(<About />);
